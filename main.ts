b65e24a46e8e88ec68f80da11bb24b31ede748b6
hernanelpan <hernan.solis@panam.ed.cr>
2025-11-19 08:54:24 -0600

    Update pxt.json, main.blocks, main.ts, images.g.jres, images.g.ts

--- main.ts
+++ main.ts
@@ -16,3 +16,4 @@ let mySprite = sprites.create(img`
     . . . . . . . . . . . . . . . . 
     . . . . . . . . . . . . . . . . 
     `, SpriteKind.Player)
+music.play(music.createSong(assets.song`cancion saika`), music.PlaybackMode.LoopingInBackground)
