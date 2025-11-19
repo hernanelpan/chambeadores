7dcb8b080063d79613bb625034ab3c7afb634726
sofiamurillo-prog <sofia.murillo@panam.ed.cr>
2025-11-19 09:04:26 -0600

    Update pxt.json, main.blocks, main.ts, tilemap.g.jres, tilemap.g.ts

--- main.ts
+++ main.ts
@@ -1,3 +1,7 @@
+scene.onOverlapTile(SpriteKind.Player, assets.tile`transparency16`, function (sprite, location) {
+    game.over(true)
+    info.startCountdown(10)
+})
 let mySprite = sprites.create(img`
     . . . . . . . . . . . . . . . . 
     . . . . . . . . . . . . . . . . 
@@ -17,3 +21,7 @@ let mySprite = sprites.create(img`
     . . . . . . . . . . . . . . . . 
     `, SpriteKind.Player)
 music.play(music.createSong(assets.song`cancion saika`), music.PlaybackMode.LoopingInBackground)
+controller.moveSprite(mySprite, 100, 100)
+tiles.setTilemap(tilemap`level1`)
+tiles.placeOnRandomTile(mySprite, assets.tile`transparency16`)
+scene.cameraFollowSprite(mySprite)
