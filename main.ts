026e53b8acf2c33b9636adbf35f89ff8eade9210
hernanelpan <hernan.solis@panam.ed.cr>
2025-11-19 09:14:55 -0600

    Update main.blocks, main.ts, images.g.jres, images.g.ts

--- main.ts
+++ main.ts
@@ -21,6 +21,7 @@ let mySprite = sprites.create(img`
     . . . . . . . . . . . . . . . . 
     `, SpriteKind.Player)
 music.play(music.createSong(assets.song`cancion saika`), music.PlaybackMode.LoopingInBackground)
+music.setVolume(5)
 controller.moveSprite(mySprite, 100, 100)
 tiles.setTilemap(tilemap`level1`)
 tiles.placeOnRandomTile(mySprite, assets.tile`transparency16`)
